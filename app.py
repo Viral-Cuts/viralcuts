754ccdc6def599166df96c4259f3d5923d557a8a
Donald Nwokoro <infect3dlab@gmail.com>
2023-07-22 18:45:19 +0100

    Feat ✨: Built Prompt Template

--- app.py
+++ app.py
@@ -2,12 +2,25 @@ from langchain.document_loaders import YoutubeLoader
 import os
 from dotenv import load_dotenv
 from langchain import OpenAI
-import streamlit as st
+from langchain.chains import LLMChain
+from langchain.prompts import (
+    ChatPromptTemplate,
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+
 
 load_dotenv()
 
-llm = OpenAI(temperature=0, streaming=True,
-             openai_api_key=os.getenv("OPENAI_API_KEY"))
+# llm = OpenAI(temperature=0, streaming=True,
+#              openai_api_key=os.getenv("OPENAI_API_KEY"))
 
 
 loader = YoutubeLoader.from_youtube_url(
@@ -17,4 +30,28 @@ loader = YoutubeLoader.from_youtube_url(
 document = loader.load()
 
 # print loader results
-print(document)
+# print(document)
+
+
+llm = OpenAI(temperature=0, streaming=True, openai_api_key=os.environ.get("OPENAI_API_KEY"))
+
+prompt = PromptTemplate(
+template = """You are the worlds short video expert.
+
+Analyze the youtube video transcript: {transcript}
+
+Find a snippet from the video transcript with high viral potential as a short, then critique it for how viral the resulting short clip will be, then and rank them out of 10. If the score is not 10, repeat the process until a snippet worthy of a score of 10 is generated.?""",
+
+input_variables = ["transcript"]
+)
+
+chain = LLMChain(llm=llm, prompt=prompt)
+
+# print()
+
+print(chain.run(document))
+
+# cd /var/www/datawb.com/public_html
+# pm2 start viralcuts.py --name viralcuts-app --interpreter python3
+# pm2 save
+# pm2 restart viralcuts-app
--- viralcuts.py
+++ viralcuts.py
@@ -8,6 +8,20 @@ from langchain.document_loaders import YoutubeLoader
 import os
 from dotenv import load_dotenv
 from langchain import OpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import (
+    ChatPromptTemplate,
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+
 
 load_dotenv()
 
@@ -18,7 +32,7 @@ app = FastAPI()
 def generate(
     video: str = Form(...),
 ):
-    # llm = OpenAI(temperature=0, streaming=True, openai_api_key=os.getenv("OPENAI_API_KEY"))
+    
     loader = YoutubeLoader.from_youtube_url(
         video, add_video_info=True
     )
@@ -26,7 +40,27 @@ def generate(
     # print(document)
 
     data = {"video": video, "document": document}
-    return data
+
+    
+    llm = OpenAI(temperature=0, streaming=True, openai_api_key=os.environ.get("OPENAI_API_KEY"))
+
+    prompt = PromptTemplate(
+    template = """You are the worlds short video expert.
+
+    Analyze the youtube video transcript: {transcript}
+
+    Find a snippet from the video transcript with high viral potential as a short, then critique it for how viral the resulting short clip will be, then and rank them out of 10. If the score is not 10, repeat the process until a snippet worthy of a score of 10 is generated.?""",
+
+    input_variables = ["transcript"]
+    )
+
+    chain = LLMChain(llm=llm, prompt=prompt)
+
+    # print()
+
+    results = chain.run(data)
+
+    return results
 
 
 if __name__ == "__main__":
