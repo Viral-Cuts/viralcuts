9d0162132025a572b29426566c8e049b037d8896
EasyAi Studio <123156899+EasyAiStudio@users.noreply.github.com>
2023-07-23 15:48:21 +1000

    Create transcripts.py

---
+++ transcripts.py
@@ -0,0 +1,61 @@
+# Multi-line string containing transcripts
+transcripts = """
+Based on the transcript provided, here are five exact sections that fit the client's requirements:
+
+1. Section 1 (147 words):
+"Now, today's stability a I decided to release that model officially so you can download it, no bad feelings. It's still the 0.9 version, and this one is not prone, so it's going to be bigger, but you can download it, you can experiment with it to your heart's content. So let's check out how that works when you go to the hacking phase page of Stability AI. You see down here two new additions: the sdxl refiner model and the sdxl base model. Of course, you need to download both of them. Now when you click on that link, you are greeted by this form here, where there is a lot of talk about what you can and can't do with that. You, of course, cannot do any commercial things with that for now because this is just for research purposes."
+
+2. Section 2 (108 words):
+"Confui is super easy to use. It has a self-contained folder that you just have to download and then open the program. So, you can see we have this link here, download the folder, unpack it; it is a 7-Zip file, so for that, you can use WinRAR or other files that support 7-Zip. Unpack that, and then you can start the program. If you want to have a more in-depth description of how to set up Confui, I have a video here for you that you should check out. Once everything is set up for your Confui, you want to go into Confui to the models folder and, in there, to the checkpoints folder, and here you put the two sdxl models."
+
+3. Section 3 (118 words):
+"The only thing you need to do here is to click here on that queue button to start the rendering. Now, this will first run through the base model and give you a preview of what the base model is rendering. Here you can see the result that I got from my prompt; it is looking very nice, but it's not refined yet, so it's not perfect at all. After that, this is automatically running for you through the refiner model, and the output will be up here, so you always see both of the results, and you can compare them. So here you can see the result that I got from the refiner model, and this is much better, much more detailed, much more realistic."
+
+4. Section 4 (118 words):
+"Don't forget to join my live stream tomorrow, where we are going to experiment with the sdxl 0.9 model. Also, I'm gonna make a new video again about how to train portraits for Laura. I did a lot of experimenting with that in recent days. I also did some photo shootings with my very nice Sony camera and really cool lens from Sigma, so we are going to look into how these models turned out, compare the photos with the results, talk about how to refine that, all these kind of awesome things. Thanks for watching, see you in my live stream, and have a nice day. Bye!"
+
+(Note: The transcript is not long enough to provide five exact sections. The fourth section above is the final section available in the transcript.)
+
+
+
+Section 1 (148 words):
+"So imagine is the founder and CEO of the AI startup stability AI he launched the company back in 2019 and he went through a couple different business ideas for it none of them really ended up generating much revenue if any and eventually Emma just kept going kept searching for a business that would work it was summer 2022 that stability really broke out had a its big sort of almost overnight viral moment and this came with the launch of the generative AI text to image generator called stable diffusion this is a model which allows you to type in a phrase anything from your imagination and can generate uh almost instantly an AI generated image."
+
+Section 2 (142 words):
+"I think one of the big unlocks was we got a fundraising pitch deck from Summer 2022 that had a list of I think about like 50 employees on there in one of the slides of the document and what I found was that there were many people that were listed on that slide that did not have stability on their LinkedIn I did not have any like publicly searchable connection to stability but they were on this document and one of the people uh was this person named Eric hallahan who went on the record and talked to us about his experiences at the company Eric had been an intern at the company he spoke through the chaos of the company a bit he had been slated to work 300 hours with a company over last summer and ultimately 181 of the hours he sent an invoice to stability in August and eight months later he had not heard back he had not been paid for it."
+
+Section 3 (115 words):
+"I contacted stabilities collaborators that uh on stable diffusion their image generator and I ended up getting in touch with the professor at the German university called Ludwig Maximilian University of Munich and turns out that this guy this professor had overseen the initial research to create the AI model that served as the basis for stable diffusion he had a lot of gripes about the way that stability had marketed itself and positioned itself as being they said in public marketing materials the company behind stable diffusion as this professor told me you know stability would sometimes take an outsized amount of credit for you know being the company behind this work and then sometimes it was only after he had reached out to the company that the company dialed it back or you know gave the university some credit."
+
+Section 4 (135 words):
+"We also got our hands on a fundraising uh pitch decks so these are like PowerPoint presentations essentially that the company presents to investors that they hope to put money into the company it claimed that it had Partnerships with organizations including the United Nations the World Bank the World Health Organization they claimed a partnership with Amazon web services that in these slide decks they described as a strategic partnership they claimed that Amazon had supplied a vast amount of AI chips at a 80 discount to the market value when I talked to Imad in January he told me that Amazon had cut stability a very attractive deal that he said came with quote unquote certain personal guarantees when I asked him for more information about this I was sitting in the room with him as well as his wife Sarah Qureshi who was the company's head of PR at the time she shot my question down and they just would not go further on that conversation point until we changed the topic."
+
+Section 5 (102 words):
+"Ahmad took to Twitter and he also wrote a blog post addressing some of the claims that we had made in the story I think stability has had some kind of uh skepticism from a certain portion of the AI Community I think even prior to us publishing the story some of the feedback we've gotten afterwards is just a um people being glad that this story is out there with concrete facts rather than you know being limited to hearsay and back Channel I think it really speaks to what's going on in AI right now because of how popular generative AI has been investors out here are just flocking to uh fund companies uh at very Loft evaluations throwing around tens of millions or hundreds of millions of dollars historically you know Silicon Valley investors have spent weeks if not months you know uh doing due diligence and researching a company and researching the founder and ensuring that there are no red flags and they're comfortable to invest I think the big question will be um with AI being as hypey as it is now um how mindful will investors be of Performing due diligence of taking a bit more time to check for potential red flags at a company before going in on an investment."
+
+
+
+Here are 5 exact sections from the transcript, each containing between 100-150 words, and with high viral potential:
+
+1. Section 1 (144 words):
+"By now, you probably realize that Chat GPT is one of the Great Leaps forward in AI. So, what I wanted to do is run you through how to use Prompt Toolbox to help you better use Chachi BT. The first thing I'm going to do is run a search. Say you want to make a new blog post, but you're a bit hungover and lacking inspiration. Just type in 'blog,' and it'll give you a bunch of possible things you could do. For example, you wanted to have a blog to explain a topic, maybe about chemistry. You can type in 'chemistry' and ask, 'Can we talk about chemistry?' Copy and paste all these prompts here. The reason why it's not just one prompt, but three prompts, is because, turns out, if you do one, two, three, you get a much better result. That's how to actually use it to do something."
+
+2. Section 2 (137 words):
+"Next, I want to show you how to Fork it. Say you've got a much better idea of how to do your prompts, or you've been experimenting with the edit button and found a better way. You can click this Fork button over here, and it will create a new prompt that you can edit. Perhaps you want to see 'You are the world's expert in chemistry.' Actually, a better thing to do would be to copy the topic here, so we'll get rid of that and put in 'topic.' You are the world's expert in 'topic,' can we talk about 'topic?' Then you hit save, and now when you copy and paste, you have the new edit there. So if we were to change this to 'chemistry,' click generate, there you go! Now you click copy, and you've got all these different things there. Okay, let's, let's part one and part two."
+
+3. Section 3 (120 words):
+"Next, I want to show you how to do a new prompt. Click new prompt and say, 'Okay, I feel like I want to make a new prompt about writing homework.' The first thing you're going to do is get the form edit, and in this particular case, the framework, you might be assigned a topic. Say, 'What is your homework topic?' And then you've got a choice, really text or text area. The other ones don't work yet, so if we just do text, because it's a short thing, if it was like a, you know, you're going to rewrite a blog post, something like that, you'd pick text area, but we're just going to use text. Then you click add, now you have your text field here."
+
+4. Section 4 (127 words):
+"Let me give you an example of what happens if you decide to add more fields. You can move them up, you can move them down, there's quite a lot of freedom here, even though it looks admittedly quite ugly. Okay, so we're going to get rid of this one, we don't need that. Next, we want to go to the prompt editor, so we'll add a prompt and you'd say, 'Alright, 500 words essay about... save.' Then you're like, 'Oh, I decided that if I do this, it's going to be the exact same as everybody else in my class, so I want to change it to be, make it in the style of A12, greater save.' And then, for extra non-looks like it came from GPT, um, we'll say, 'Make the tone conversational.'"
+
+5. Section 5 (119 words):
+"Next time, when you're one hour before your homework is about to be submitted, you can go to your prompt and say, 'Oh, what's my topic? Oh, it's gonna be mathematics.' Okay, quickly go and make a new prompt about mathematics. Yeah, I'm in a hurry, and then it's going to be a 500-word thing, and you're gonna make it look maybe not like it just came from judges, maybe just a little bit different from everybody else's. So when the teacher looks at your homework, you're gonna be like, 'Oh, this is slightly different from everybody else's. Maybe they didn't use Chat GPT.' Anyway, you can also edit the form here and go back to your thing, edit the prompt. You can also explore what other people have come up with, and as you can see, I create Forks. If you think you've got a better way of doing it, there you go."
+"""
+
+# Split the multi-line string into individual transcripts
+transcripts_array = transcripts.strip().split('\n\n\n\n')
+
+# Printing individual transcripts in the array
+for i, transcript in enumerate(transcripts_array, 1):
+    print(f"Transcript {i}: {transcript.strip()}")
